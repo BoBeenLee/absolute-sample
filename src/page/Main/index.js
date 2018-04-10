55282afab877b3a391df464bc6919e3398b89ab4
bobinlee <cultist_tp@naver.com>
2018-04-11 00:45:46 +0900

    initial create-react-app.

---
+++ src/page/Main/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Layout from '../Layout';
+
+class Main extends Component {
+    render() {
+        return (
+            <Layout />
+        );
+    }
+}
+
+export default Main;
